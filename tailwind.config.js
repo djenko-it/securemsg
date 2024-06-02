127af2a2d548dc108bebd57314cd5f91e360d697
djenko-it <djenko-it@protonmail.com>
2024-06-02 15:48:59 +0200

    cfg css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: ['./templates/**/*.html', './static/**/*.js'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
