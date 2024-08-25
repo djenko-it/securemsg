8793d60cd13b1dbc1a57332b67cff7424f38af34
djenko-it <djenko-it@protonmail.com>
2024-08-25 22:58:13 +0200

    rb

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
-    DATABASE = os.environ.get('DATABASE_URL', '/app/messages.db')
-    # Autres configurations
